efc23360d9341cbe15dc0eaa9aaf3c7388a5557b
Rick Hwang <rick_kyhwang@hotmail.com>
2023-07-16 14:03:08 +0800

    update on 20230716-1403, comment:

--- run.sh
+++ run.sh
@@ -4,12 +4,12 @@ source mkdocs_config.sh
 
 docker pull $MKDOCS_IMAGE
 
-(sleep 5 && {
-  echo -e "${COLOR_GREEN}=========================================="
-  echo -e "${COLOR_GREEN}使用完畢請記得按下 CTRL-C 關閉 container。"
-  echo -e "${COLOR_GREEN}=========================================="
-  echo -e "${COLOR_NONE}"
-}) &
+# (sleep 5 && {
+#   echo -e "${COLOR_GREEN}=========================================="
+#   echo -e "${COLOR_GREEN}使用完畢請記得按下 CTRL-C 關閉 container。"
+#   echo -e "${COLOR_GREEN}=========================================="
+#   echo -e "${COLOR_NONE}"
+# }) &
 
 docker run --rm \
   -v $PWD:/docs-src \
